5633d9e1c68e26dd70234b716a47ad8eee810e6c
Jamez Mayke <jamezmayke@gmail.com>
2023-09-18 18:30:46 -0300

    Finalizando ranking

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var heroName = "Jamez";
+var experienceHero = 893;
+
+if (experienceHero >= 10001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Radiante **`);
+} else if (experienceHero >= 9001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Imortal **`);
+} else if (experienceHero >= 8001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Ascendente **`);
+} else if (experienceHero >= 7001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Platina **`);
+} else if (experienceHero >= 6001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Ouro **`);
+} else if (experienceHero >= 2001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Prata **`);
+} else if (experienceHero >= 1001) {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Bronze **`);
+} else {
+  console.log(`O Herói de nome ** ${heroName} ** está no nível de ** Ferro **`);
+}
